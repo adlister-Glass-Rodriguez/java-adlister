eaed6e9d51134c69a0668e41bb46f5c384c5d3ee
Raymond Glass <raymond.allen.glass@gmail.com>
2023-06-16 13:35:08 -0500

    working files

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -3,11 +3,27 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/profile.jsp").forward(request, response);
+
+        // if user isnt logged in, send them to login page:
+
+        HttpSession session = request.getSession();
+        String isLoggedIn = (String) session.getAttribute("isLoggedIn");
+        if(isLoggedIn == null) {
+            response.sendRedirect("/login");
+            return;
+        }
+        request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+
+//        String username = (String) request.getSession().getAttribute("user");
+//
+//        request.setAttribute("username", username);
+//        request.getRequestDispatcher("/profile.jsp").forward(request, response);
+
     }
 }
--- src/main/webapp/WEB-INF/profile.jsp
+++ src/main/webapp/WEB-INF/profile.jsp
@@ -1,15 +1,24 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <jsp:include page="../partials/head.jsp">
+    <jsp:include page="../WEB-INF/partials/head.jsp">
         <jsp:param name="title" value="Your Profile" />
     </jsp:include>
 </head>
 <body>
-    <jsp:include page="../partials/navbar.jsp" />
+    <jsp:include page="../WEB-INF/partials/navbar.jsp" />
 
     <div class="container">
-        <h1>Viewing your profile.</h1>
+            <div class="container">
+                <h1>Welcome, <%= request.getAttribute("username") %>!</h1>
+                <h2>Viewing your profile.</h2>
+            </div>
+
+        <form action="/logout" method="POST">
+            <input type="submit" class="btn btn-primary btn-block" value="Log Out">
+        </form>
+
     </div>
 
 </body>
